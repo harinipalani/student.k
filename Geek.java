6974063bf5255d6be3f869d59ce2c6939a474ec2
harinipalaniz <114638109+harinipalaniz@users.noreply.github.com>
2022-09-29 10:03:49 +0530

    Create Geek.java

---
+++ Geek.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class geek
+{
+static void
+Vowel_or_Consonant(char y)
+{
+if(y=='a'||y=='e'||y=='i'||y=='o'||y=='u')
+System.out.println("it is an Vowel.");
+else
+	System.out.println("it is a Consonant.");
+}
+static  public void main(String[]args)
+{
+Vowel_or_Consonant('b');
+Vowel_or_Consonant('u');
+}
+}
