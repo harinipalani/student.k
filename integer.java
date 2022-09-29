f83115b4c57156a1bcdc040023528a3b753fbead
harinipalaniz <114638109+harinipalaniz@users.noreply.github.com>
2022-09-29 10:35:53 +0530

    Create integer.java

---
+++ integer.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ 
+class GFG 
+{
+
+    public static void main(String[] args)
+    {
+        int i = 65;
+             char ch = (char)i;
+             System.out.println(ch);
+    }
+}
+Footer
+© 2022 GitHub, Inc.
